71ce726b93b8f556507858ea43d2f1ea27b35695
German Ostaszynski <german92@gmail.com>
2015-09-21 17:17:50 -0600

    connected angular with ruby

--- app/cotrollers/HeroController.js
+++ app/cotrollers/HeroController.js
@@ -1,7 +1,11 @@
 angular.module('sandbox.controllers')
 
-.controller('HeroController', [ 'heroService', function(heroService) {
-  this.superHeroesData = heroService.superHeroesData();
-  var a = heroService.test();
+.controller('HeroController', ['heroService', function(heroService) {
+  var ctrl = this;
+
+  ctrl.superHeroesData = [];
+  heroService.superHeroesData().success(function (argument) {
+    ctrl.superHeroesData = argument;
+  });
 
 } ]);
--- app/directives/displayHeroes/displayHeroes.js
+++ app/directives/displayHeroes/displayHeroes.js
@@ -8,14 +8,18 @@ angular.module('sandbox.directives')
     restrict: 'E',
     templateUrl: './directives/displayHeroes/displayHeroes.html',
     controller: ['$scope', function($scope) {
-
-      $scope.callSuperHero = function(hero) {
-        alert('Your heros real name is... ' + hero.realName);
-      };
-
       $scope.toggleOnCall = function(hero) {
         hero.onCall = !hero.onCall;
       };
     }]
   };
 }]);
+
+// $scope.callSuperHero = function(hero) {
+//   alert('Your heros real name is... ' + hero.realName);
+// };
+
+
+// $scope.$watch('hero', function(newValue, oldValue) {
+//   console.log(newValue, oldValue);
+// });
